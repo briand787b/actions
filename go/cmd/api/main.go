f18ff00bcdd3237a160b9512d5c66dc58365a49b
Brian D'Ostilio <briand787b@gmail.com>
2022-02-14 09:50:39 -0500

    needed to move test output files into GOROOT for go tooling to work on them

--- go/cmd/api/main.go
+++ go/cmd/api/main.go
@@ -7,6 +7,7 @@ import (
 	"actions/internal/sum"
 )
 
+// this should obv be an API, but I'll keep it simple since this is a demo
 func main() {
 	fmt.Printf("4 + 1 = %d\n", sum.Sum(4, 1))
 	fmt.Printf("4 / 1 = %d\n", div.CustomDivide(4, 1))
